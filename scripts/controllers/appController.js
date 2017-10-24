a93bb37d5176cf695a45a953ac721acb87255661
brando mota <brandomb@bol.com.br>
2017-10-24 14:46:45 -0400

    always look on the bright side of the bugs....

--- scripts/controllers/appController.js
+++ scripts/controllers/appController.js
@@ -1,12 +1,23 @@
-(function () {
-    'use strict';
- }());
 
- angular.module('botApp').controller('appCtrl',["$scope", "botService",
- function($scope, botService){
+ angular.module('botApp').controller('appCtrl',["$scope", "botService","toastr",
+ function($scope, botService, toastr){
+
+    $scope.listaMsgs = [];
+
+    $scope.teste = function(){
+        console.log("ojjjjj")
+    }
 
     $scope.conectarBot = function(){
-        
+        console.log("oks")
+        botService.retornarPrimeiraMensagem()
+        .then(function(retorno){
+            console.log(retorno)
+            var msg = {"Texto": retorno.data, "Send": 2};
+            $scope.listaMsgs.push(msg);
+        }).catch(function(){
+            toastr.error('erro ao conectar...');
+        });
     };    
 
  }]);
--- scripts/services/botService.js
+++ scripts/services/botService.js
@@ -1,3 +1,16 @@
-angular.module('botApp').service('botService',function(){
+(function () {
+    'use strict';
+}());
 
-});
+angular.module('botApp').service('botService',['$http', function ($http) {
+
+    return {
+        retornarPrimeiraMensagem: function () {
+            return $http({
+                url: 'http://localhost:3000',
+                method: 'GET'
+            })
+        }
+    }
+
+}]);
